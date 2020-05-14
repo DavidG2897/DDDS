51330542bd2d3348f0f11fb874b94a4b4c44afd5
David Garcia <david.g.2897@gmail.com>
2020-05-14 00:32:32 -0500

    added map to logged home, emergency list + other stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+	if current_user
+	  @user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies
+	end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
-module ApplicationHelper
+module ApplicationHelper	
+	def get_google_api_req
+		"https://maps.googleapis.com/maps/api/js?key=" + get_google_api_key
+	end
+	
+	def get_google_api_key
+		Rails.application.credentials.google[:api_key]
+	end
 end

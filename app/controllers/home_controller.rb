11b389b9c3500f7c2bf5035aea1eb3681ee5ae6e
David Garcia <david.g.2897@gmail.com>
2020-05-16 17:34:27 -0500

    homepage, added total user ems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
 	if current_user
 	  if !current_user.device.nil?
-	    @user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies
+	    @user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies.order(created_at: :desc)
+		@user_em_cnt = @user_emergencies.length()
 	  end
 	  @all_locs = ActiveRecord::Base.connection.execute("SELECT lat, long as lng, COUNT(lat) as cnt FROM Locations GROUP BY lat, long")
 	end

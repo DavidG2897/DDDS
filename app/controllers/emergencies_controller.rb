c46211e65f8dfb79698ef9c862e7bdb819a4f4f7
David Garcia <david.g.2897@gmail.com>
2020-05-17 22:05:52 -0500

    fix

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -8,14 +8,14 @@ class EmergenciesController < ApplicationController
 		#FIXME: this does not re-render map when coming from sidebar, weird... check sidebar
 			redirect_to root_path, :alert => 'Please register your device to check your emergencies'
 		else
-			@user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies.order(created_at: :desc)
+			@user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies.order(created_at: :asc)
 			@user_em_cnt = @user_emergencies.length()
 			
 			em = params[:em]
 			ad_id = AdminDevice.find_by(serial: current_user.device.dispid).id
 			query = "SELECT DISTINCT ON (emergency_id) * FROM (SELECT e.admin_device_id, l.lat, l.long as lng, e.created_at as e_ca, l.created_at, l.emergency_id FROM Emergencies e, Locations l WHERE e.id = l.emergency_id ORDER BY e.created_at, l.created_at ASC) subq WHERE admin_device_id = " + ad_id.to_s + " AND emergency_id = " + em
 			@first_loc_per_em = ActiveRecord::Base.connection.execute(query)
-			query = "SELECT lat, long as lng, created_at FROM Locations WHERE emergency_id = " + em
+			query = "SELECT lat, long as lng, created_at FROM Locations WHERE emergency_id = " + em + " ORDER BY created_at ASC"
 			@em_related_locs = ActiveRecord::Base.connection.execute(query)
 			@emergency = Emergency.find(em.to_i)
 		end

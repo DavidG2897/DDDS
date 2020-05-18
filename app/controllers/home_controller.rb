0c6546ce01243b67db6fefdd1abd3e849043f82e
David Garcia <david.g.2897@gmail.com>
2020-05-17 19:56:25 -0500

    map query fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
 	    @user_emergencies = AdminDevice.find_by(serial: current_user.device.dispid).emergencies.order(created_at: :desc)
 		@user_em_cnt = @user_emergencies.length()
 	  end
-	  @all_locs = ActiveRecord::Base.connection.execute("SELECT lat, long as lng, COUNT(lat) as cnt FROM Locations GROUP BY lat, long")
+	  @all_locs = ActiveRecord::Base.connection.execute("SELECT DISTINCT ON (emergency_id) * FROM (SELECT l.lat, l.long as lng, e.created_at as e_ca, l.created_at, l.emergency_id FROM Emergencies e, Locations l WHERE e.id = l.emerge
+ncy_id ORDER BY e.created_at, l.created_at ASC) subq")
 	  @all_em_cnt = Emergency.all.length()
 	end
   end
